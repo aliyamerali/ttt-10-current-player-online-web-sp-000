607bd583a5d20f17eba22f5746dbeef1ded5b97a
Aliya Merali <aliya.merali@flatironschool.com>
2020-05-13 01:27:57 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,7 +1,7 @@
 def turn_count(board)
   turns = 0
   board.each do |space|
-    space == "X" || space == "O" ? turns += 1 : bil
+    space == "X" || space == "O" ? turns += 1 : nil
   end
   return turns
 end
