7ea8ec86ed95e063f704c4597f9730da3945d012
Aliya Merali <aliya.merali@flatironschool.com>
2020-05-13 01:25:31 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -9,6 +9,6 @@ def turn_count(board)
 end
 
 def current_player(board)
-  turn_count(board).even? =~ true ? player = "X" : player = "O"
+  turn_count(board).even? == true ? player = "X" : player = "O"
   return player
 end
